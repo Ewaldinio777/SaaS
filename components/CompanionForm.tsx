c72896462c8a2cc35207c290150c69359a553b3e
Eduardo Barrios <edu.ve1524@gmail.com>
2026-07-13 17:22:33 -0400

    Form done for build a companion done

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -16,28 +16,35 @@ import {
 } from "@/components/ui/card";
 import {
   Field,
-  FieldDescription,
   FieldError,
   FieldGroup,
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Textarea } from "./ui/textarea";
+
+// 1. Definimos el esquema
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Companion is required." }),
+  subject: z.string().min(1, { message: "Subject is required." }),
+  topic: z.string().min(1, { message: "Topic is required." }),
+  voice: z.string().min(1, { message: "Voice is required." }),
+  style: z.string().min(1, { message: "Style is required." }),
+  duration: z.number().min(1, { message: "Duration is required." }),
+});
+
+type FormValues = z.infer<typeof formSchema>;
 
 const CompanionForm = () => {
-  const formSchema = z.object({
-    name: z.string().min(1, { message: "Companion is required." }),
-    subject: z.string().min(1, { message: "Subject is required." }),
-    topic: z.string().min(1, { message: "Topic is required." }),
-    voice: z.string().min(1, { message: "Voice is required." }),
-    style: z.string().min(1, { message: "Style is required." }),
-    duration: z.coerce.number().min(1, { message: "Duration is required." }),
-  });
-
-  const form = useForm<
-    z.input<typeof formSchema>,
-    unknown,
-    z.output<typeof formSchema>
-  >({
+  const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
@@ -49,41 +56,33 @@ const CompanionForm = () => {
     },
   });
 
-  const onSubmit = (values: z.output<typeof formSchema>) => {
+  const onSubmit = (values: FormValues) => {
     console.log(values);
   };
 
   return (
     <Card className="w-full sm:max-w-md">
-      <CardHeader>
-        <CardTitle>Profile Settings</CardTitle>
-        <CardDescription>
-          Update your profile information below.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form id="form-rhf-input" onSubmit={form.handleSubmit(onSubmit)}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <CardHeader>
+          <CardTitle>Create a Companion</CardTitle>
+          <CardDescription>Complete the information below.</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          {/* Name */}
           <FieldGroup>
             <Controller
               name="name"
               control={form.control}
               render={({ field, fieldState }) => (
                 <Field data-invalid={fieldState.invalid}>
-                  <FieldLabel htmlFor="form-rhf-input-username">
-                    Username
-                  </FieldLabel>
+                  <FieldLabel htmlFor="companion-name">Name</FieldLabel>
                   <Input
                     {...field}
-                    id="form-rhf-input-username"
+                    id="companion-name"
                     aria-invalid={fieldState.invalid}
-                    placeholder="shadcn"
-                    autoComplete="username"
+                    placeholder="Enter the companion name"
+                    autoComplete="name"
                   />
-                  <FieldDescription>
-                    This is your public display name. Must be between 3 and 10
-                    characters. Must only contain letters, numbers, and
-                    underscores.
-                  </FieldDescription>
                   {fieldState.invalid && (
                     <FieldError errors={[fieldState.error]} />
                   )}
@@ -91,18 +90,178 @@ const CompanionForm = () => {
               )}
             />
           </FieldGroup>
-        </form>
-      </CardContent>
-      <CardFooter>
-        <Field orientation="horizontal">
-          <Button type="button" variant="outline" onClick={() => form.reset()}>
-            Reset
-          </Button>
-          <Button type="submit" form="form-rhf-input">
-            Save
-          </Button>
-        </Field>
-      </CardFooter>
+
+          {/* Subject */}
+          <FieldGroup>
+            <Controller
+              name="subject"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="companion-subject">Subject</FieldLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger
+                      id="companion-subject"
+                      className="input capitalize"
+                    >
+                      <SelectValue placeholder="Select the subject" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        {subjects.map((subject) => (
+                          <SelectItem
+                            value={subject}
+                            key={subject}
+                            className="capitalize"
+                          >
+                            {subject}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+
+          {/* Topic */}
+          <FieldGroup>
+            <Controller
+              name="topic"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="companion-topic">
+                    What should the companion help with?
+                  </FieldLabel>
+                  <Textarea
+                    {...field}
+                    id="companion-topic"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Ex. Derivatives & Integrals"
+                    autoComplete="topic"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+
+          {/* Voice */}
+          <FieldGroup>
+            <Controller
+              name="voice"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="companion-voice">Voice</FieldLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger id="companion-voice" className="input">
+                      <SelectValue placeholder="Select the voice" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="male">Male</SelectItem>
+                        <SelectItem value="female">Female</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+
+          {/* Style */}
+          <FieldGroup>
+            <Controller
+              name="style"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="companion-style">Style</FieldLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger id="companion-style" className="input">
+                      <SelectValue placeholder="Select the style" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="formal">Formal</SelectItem>
+                        <SelectItem value="casual">Casual</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+
+          {/* Duration */}
+          <FieldGroup>
+            <Controller
+              name="duration"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="companion-duration">Duration</FieldLabel>
+                  <Input
+                    {...field}
+                    value={field.value ?? ""}
+                    onChange={(e) => {
+                      const val = e.target.value;
+                      field.onChange(val === "" ? "" : Number(val));
+                    }}
+                    type="number"
+                    id="companion-duration"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter the duration"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </CardContent>
+        <CardFooter>
+          <Field className="flex w-full gap-2" orientation="horizontal">
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => form.reset()}
+            >
+              Reset
+            </Button>
+            <Button type="submit" className="flex-1 cursor-pointer">
+              Build Your Companion
+            </Button>
+          </Field>
+        </CardFooter>
+      </form>
     </Card>
   );
 };
