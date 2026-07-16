c74b7e5ec43c00cbdeee04a309f250e2407e5c0e
Eduardo Barrios <edu.ve1524@gmail.com>
2026-07-16 12:38:19 -0400

    Controller implement for creating a companion, filters for looking the companions created

---
+++ components/SearchInput.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const SearchInput = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("topic") || "";
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+      const currentParams =
+        typeof window !== "undefined" ? window.location.search : "";
+
+      if (!searchQuery) {
+        newUrl = removeKeysFromUrlQuery({
+          params: currentParams,
+          keysToRemove: ["topic"],
+        });
+      } else {
+        newUrl = formUrlQuery({
+          params: currentParams,
+          key: "topic",
+          value: searchQuery,
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchQuery, router]);
+
+  return (
+    <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
+      <Image src="/icons/search.svg" alt="search" width={15} height={15} />
+      <input
+        placeholder="Search companions ..."
+        className="outline-none"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
---
+++ components/SubjectFilter.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { subjects } from "@/constants";
+
+const SearchInput = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("subject") || "";
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  useEffect(() => {
+    let newUrl = "";
+    const currentParams =
+      typeof window !== "undefined" ? window.location.search : "";
+
+    if (!searchQuery) {
+      newUrl = removeKeysFromUrlQuery({
+        params: currentParams,
+        keysToRemove: ["subject"],
+      });
+    } else {
+      newUrl = formUrlQuery({
+        params: currentParams,
+        key: "subject",
+        value: searchQuery,
+      });
+    }
+
+    router.replace(newUrl, { scroll: false });
+  }, [searchQuery, router]);
+
+  return (
+    <Select
+      value={searchQuery}
+      onValueChange={(value) => setSearchQuery(value ?? "")}
+    >
+      <SelectTrigger id="companion-subject" className="input capitalize">
+        <SelectValue placeholder="Select the subject" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {subjects.map((subject) => (
+            <SelectItem value={subject} key={subject} className="capitalize">
+              {subject}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default SearchInput;
